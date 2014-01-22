5b3d46e46f0fed93a8f6b1abce1cffa5851ab88d
Ashish Agarwal <agarwal1975@gmail.com>
2014-01-22 16:58:58 -0500

    travis.sh: fix wrong package name

--- bin/travis.sh
+++ bin/travis.sh
@@ -1,4 +1,4 @@
-OPAM_DEPENDS="ocamlfind omake core.$CORE_VERSION cfstream lwt.syntax async"
+OPAM_DEPENDS="ocamlfind omake core.$CORE_VERSION cfstream lwt async"
 
 case "$OCAML_VERSION,$OPAM_VERSION" in
 3.12.1,1.0.0) ppa=avsm/ocaml312+opam10 ;;
