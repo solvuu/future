c4ff4d2dae7784a021b70f050b2f6ee8504a3401
Ashish Agarwal <agarwal1975@gmail.com>
2016-08-15 15:09:22 -0400

    myocamlbuild: update wrt latest solvuu-build

--- myocamlbuild.ml
+++ myocamlbuild.ml
@@ -9,7 +9,7 @@ let lib ?findlib_deps ?internal_deps ?ml_files lib_name : Project.item =
   Project.lib (sprintf "%s_%s" project_name lib_name)
     ~pkg:(sprintf "%s.%s" project_name lib_name)
     ~dir:(sprintf "lib/%s" lib_name)
-    ~pack_name:(sprintf "%s_%s" project_name lib_name)
+    ~style:(`Pack  (sprintf "%s_%s" project_name lib_name))
     ?findlib_deps
     ?internal_deps
     ?ml_files
